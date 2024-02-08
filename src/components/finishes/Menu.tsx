e0ebd454f0f0f63dd01cca27647a7a9a60602ba9
lucas <frajola83@gmail.com>
2024-02-08 14:54:52 -0300

    correção final, acabamentos

--- src/components/finishes/Menu.tsx
+++ src/components/finishes/Menu.tsx
@@ -49,10 +49,11 @@ export const Menu: React.FC<MenuProps> = ({ category_slug }) => {
       </MenuSection>
       <MenuSectionTitle>{t("pages.finishes.menuAny")}</MenuSectionTitle>
       <MenuSection>
+        <MenuItem category="concreto" currentCategory={category_slug}>{t("finishes.concrete")}</MenuItem>
         <MenuItem category="couro-natural" currentCategory={category_slug}>{t("finishes.naturalLeather")}</MenuItem>
         <MenuItem category="couro-rustico" currentCategory={category_slug}>{t("finishes.rusticLeather")}</MenuItem>
-        <MenuItem category="MDF-laminas-sinteticas" currentCategory={category_slug}>{t("finishes.syntheticVeneer")}</MenuItem>
         <MenuItem category="malha-knit" currentCategory={category_slug}>{t("finishes.fabricKnit")}</MenuItem>
+        <MenuItem category="marmore" currentCategory={category_slug}>{t("finishes.marble")}</MenuItem>
         <MenuItem category="palha-natural" currentCategory={category_slug}>{t("finishes.naturalStraw")}</MenuItem>
         <MenuItem category="pintura-metalica" currentCategory={category_slug}>{t("finishes.metallicPainting")}</MenuItem>
         <MenuItem category="recouro" currentCategory={category_slug}>{t("finishes.reconstitutedLeather")}</MenuItem>
